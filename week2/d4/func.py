06a4dddab4e97487e4880d00d2e35e6547989c10
sergeyieffe <sergeyieffe@gmail.com>
2024-03-06 10:39:16 +0200

    upd

--- week2/d4/func.py
+++ week2/d4/func.py
@@ -1,7 +1,4 @@
 #ex1
-#Write a function called display_message() that prints one sentence 
-#telling everyone what you are learning in this course.
-#Call the function, and make sure the message displays correctly.
 
 def display_message():
     print('Learning Python')
