9cce0ee8071761a460ef2855ef96e118f7fc0dc3
sergeyieffe <sergeyieffe@gmail.com>
2024-03-06 13:24:21 +0200

    Update func.py

--- week2/d4/func.py
+++ week2/d4/func.py
@@ -26,10 +26,6 @@ describe_city('Madrid','Spain')
 
 
 #ex4
-#Create a function that accepts a number between 1 and 100 and generates another 
-#number randomly between 1 and 100. Use the random module.
-#Compare the two numbers, if it’s the same number, display a success message, 
-#otherwise show a fail message and display both numbers.
 
 from random import randint
 
@@ -40,4 +36,152 @@ def rand(num):
     else:
         print(f'Fail with {num} and {randnum}')
 
-rand(10)            
+rand(10)
+
+
+
+#ex5
+
+def make_shirt(size='L', text='I love Python'):
+    if size != 'M' or size != 'L':
+        text
+    print(f'The size of the shirt is {size} and the text is {text}')
+
+make_shirt(size='S',text='Nike')
+make_shirt(size='M')
+make_shirt()
+
+
+
+#6
+#Using this list of magician’s names
+#magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+#Create a function called show_magicians(), which prints the name of each magician in the list.
+#Write a function called make_great() that modifies the original list of magicians by adding 
+#the phrase "the Great" to each magician’s name.
+#Call the function make_great().
+#Call the function show_magicians() to see that the list has actually been modified.
+
+magician_names = ['Harry Houdini', 'David Blaine', 'Criss Angel']
+
+def show_magicians(magicians = magician_names):
+    print(magicians)
+
+show_magicians()
+
+def make_great(great):
+    for name in great:
+        return print('Great ' + name)
+
+make_great(magician_names)        
+
+### why only one name in Greats ??
+
+
+
+
+#ex7
+#Create a function called get_random_temp().
+#This function should return an integer between -10 and 40 degrees (Celsius), 
+#selected at random. Test your function to make sure it generates expected results.
+#
+#Create a function called main().
+#Inside this function, call get_random_temp() to get a temperature, and store its value in a variable.
+#Inform the user of the temperature in a friendly message, eg. “The temperature 
+#right now is 32 degrees Celsius.”
+#
+#Let’s add more functionality to the main() function. Write some friendly advice 
+#relating to the temperature:
+#below zero (eg. “Brrr, that’s freezing! Wear some extra layers today”)
+#between zero and 16 (eg. “Quite chilly! Don’t forget your coat”)
+#between 16 and 23
+#between 24 and 32
+#between 32 and 40
+#
+#Change the get_random_temp() function:
+#Add a parameter to the function, named ‘season’.
+#Inside the function, instead of simply generating a random number between -10 and 40, 
+#set lower and upper limits based on the season, eg. if season is ‘winter’, 
+#temperatures should only fall between -10 and 16.
+#Now that we’ve changed get_random_temp(), let’s change the main() function:
+#Before calling get_random_temp(), we will need to decide on a season, so that 
+#we can call the function correctly. Ask the user to type in a season - ‘summer’, 
+#‘autumn’ (you can use ‘fall’ if you prefer), ‘winter’, or ‘spring’.
+#Use the season as an argument when calling get_random_temp().
+#
+#Bonus: Give the temperature as a floating-point number instead of an integer.
+#Bonus: Instead of asking for the season, ask the user for the number 
+#of the month (1 = January, 12 = December). Determine the season according to the month.
+
+
+def get_random_temp(season):
+    if season == 'winter':
+        rtemp = randint(-10,10)
+    if season == 'spring' or season == 'autumn':
+        rtemp = randint(10,20)
+    if season == 'summer':
+        rtemp = randint(20,40)
+    return rtemp
+
+print(get_random_temp('winter'))
+
+def main(temp):
+    tempc = int(temp)
+    print(f'The temperature right now is {tempc} degrees Celsius.')
+    if tempc < 0:
+        print('Brrr, that\'s freezing! Wear some extra layers today')
+    if tempc >=0 and tempc < 16:
+        print('Quite chilly! Don\'t forget your coat')
+    if tempc >=16 and tempc < 32:
+        print('It\'s a new t-shirt day')
+    if tempc >=32 and tempc < 40:
+        print('I\'ll stay at home')    
+
+
+main(get_random_temp('summer'))   
+
+#float......
+
+
+
+#ex8
+#This project allows users to take a quiz to test their Star Wars knowledge.
+#The number of correct/incorrect answers are tracked and the user receives 
+#different messages depending on how well they did on the quiz.
+#
+#Here is an array of dictionaries, containing those questions and answers
+#
+#Create a function that asks the questions to the user, and check his answers. 
+#Track the number of correct, incorrect answers. Create a list of wrong_answers
+#Create a function that informs the user of his number of correct/incorrect answers.
+#Bonus : display to the user the questions he answered wrong, his answer, 
+#and the correct answer.
+#If he had more then 3 wrong answers, ask him to play again.
+
+
+data = [
+    {
+        "question": "What is Baby Yoda's real name?",
+        "answer": "Grogu"
+    },
+    {
+        "question": "Where did Obi-Wan take Luke after his birth?",
+        "answer": "Tatooine"
+    },
+    {
+        "question": "What year did the first Star Wars movie come out?",
+        "answer": "1977"
+    },
+    {
+        "question": "Who built C-3PO?",
+        "answer": "Anakin Skywalker"
+    },
+    {
+        "question": "Anakin Skywalker grew up to be who?",
+        "answer": "Darth Vader"
+    },
+    {
+        "question": "What species is Chewbacca?",
+        "answer": "Wookiee"
+    }
+]
