fd2813fd824d999a61c627af5d90bdb70494b11e
sergeyieffe <sergeyieffe@gmail.com>
2024-03-06 10:59:28 +0200

    Update func.py

--- week2/d4/func.py
+++ week2/d4/func.py
@@ -12,4 +12,32 @@ display_message()
 def favorite_book(title):
     print('One of my fav books is' + title)
 
-favorite_book('Python Basics')    
+favorite_book('Python Basics')
+
+
+
+#ex3
+
+def describe_city(city, country):
+    print(f'{city} is in {country}')
+
+describe_city('Madrid','Spain')
+
+
+
+#ex4
+#Create a function that accepts a number between 1 and 100 and generates another 
+#number randomly between 1 and 100. Use the random module.
+#Compare the two numbers, if it’s the same number, display a success message, 
+#otherwise show a fail message and display both numbers.
+
+from random import randint
+
+def rand(num):
+    randnum = randint(1, 100)
+    if num == randnum:
+        print('Success!')
+    else:
+        print(f'Fail with {num} and {randnum}')
+
+rand(10)            
