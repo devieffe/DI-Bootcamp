83052c8567c804b0b4a2f306a19d83f225c25dca
sergeyieffe <sergeyieffe@gmail.com>
2024-03-06 10:44:38 +0200

    upd

--- week2/d4/func.py
+++ week2/d4/func.py
@@ -4,3 +4,12 @@ def display_message():
     print('Learning Python')
 
 display_message()    
+
+
+
+#ex2
+
+def favorite_book(title):
+    print('One of my fav books is' + title)
+
+favorite_book('Python Basics')    
